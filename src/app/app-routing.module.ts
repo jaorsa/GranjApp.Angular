d2af81b0d11509e6c618730401306b3cc6d9b8a2
Bety <A01732177@itesm.mx>
2019-11-26 13:50:01 -0600

    mmm ia c rompio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,15 +5,15 @@ import {LoginPageComponent} from './components/login-page/login-page.component';
 import {RegisterPageComponent} from './components/register-page/register-page.component';
 import {AccessPageComponent} from './components/access-page/access-page.component';
 import {NotFoundPageComponent} from './components/not-found-page/not-found-page.component';
-import {AnimalPageComponent } from './components/animal-page/animal-page.component';
-import {TeamPageComponent } from "./components/team-page/team-page.component";
+import { DashboardPageComponent } from './components/dashboard-page/dashboard-page.component';
+import { LeftBarComponent } from './components/left-bar/left-bar.component';
+
 const routes: Routes = [
   {path: '', component: HomePageComponent },
-  {path: 'login', component: LoginPageComponent },
+  /*{path: 'login', component: LoginPageComponent },
   {path: 'register', component: RegisterPageComponent },
-  {path: 'access', component: AccessPageComponent },
-  {path: 'team', component: TeamPageComponent },
-  {path: 'animal', component: AnimalPageComponent },
+  {path: 'access', component: AccessPageComponent },*/
+  {path: 'dashboard', component: DashboardPageComponent },
   {path: '**', component: NotFoundPageComponent }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,12 +15,12 @@ import { AppComponent } from './app.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { RegisterPageComponent } from './components/register-page/register-page.component';
-import { AnimalPageComponent } from './components/animal-page/animal-page.component';
-import { TeamPageComponent } from "./components/team-page/team-page.component";
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { AccessPageComponent } from './components/access-page/access-page.component';
 import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
-import { AuthService } from './services/auth.service';      
+import { DashboardPageComponent } from './components/dashboard-page/dashboard-page.component';
+import { LeftBarComponent } from './components/left-bar/left-bar.component';
+import { AuthService } from './services/auth.service';
 
 @NgModule({
   declarations: [
@@ -30,20 +30,20 @@ import { AuthService } from './services/auth.service';
     RegisterPageComponent,
     LoginPageComponent,
     AccessPageComponent,
-    AnimalPageComponent,
-    TeamPageComponent,
-    NotFoundPageComponent
+    NotFoundPageComponent,
+    DashboardPageComponent,
+    LeftBarComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    FormsModule, 
+    FormsModule,
     AngularFireAuthModule, // imports firebase/auth, only needed for auth features
     AngularFireModule.initializeApp(environment.firebaseConfig)
     //AngularFirestoreModule, // imports firebase/firestore, only needed for database features
-    
+
   ],
   providers: [AuthService],
   bootstrap: [AppComponent]
