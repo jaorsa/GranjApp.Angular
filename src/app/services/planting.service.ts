718c5fec98dd7ddbb2e117860770d47b42076844
A01172971 <A01172971@itesm.mx>
2019-11-27 12:35:07 -0600

    Create planting.service.ts

---
+++ src/app/services/planting.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Planting } from '../interfaces/planting';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlantingService {
+
+  constructor(private http: HttpClient) { }
+
+  getAllPlantings(){
+    const path = ' http://granjapp2.appspot.com/planting';
+    return this.http.get<Planting[]>(path);
+  }
+
+  getPlanting(id){
+    const path = 'http://granjapp2.appspot.com/planting/';
+    return this.http.get<Planting>(path + id);
+  }
+
+  createPlanting(planting:Planting){
+    const path = ' http://granjapp2.appspot.com/planting/';
+    return this.http.post(path, planting)
+  }
+  
+  updatePlanting(id, planting:Planting){
+    const path = ' http://granjapp2.appspot.com/planting/';
+    return this.http.put(path + id,planting);
+  }
+
+  deletePlanting(id){
+    const path = 'http://granjapp2.appspot.com/planting/';
+    return this.http.delete<Planting>(path + id);
+  }
+
+
+
+
+}
