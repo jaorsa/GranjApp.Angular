64dcb6ac2fe33eeb2a3729516fd91e8203813e3c
A01172971 <A01172971@itesm.mx>
2019-11-28 13:28:47 -0600

    Create animal.ts

---
+++ src/app/interfaces/animal.ts
@@ -0,0 +1,10 @@
+export interface Animal {
+    name: string;
+    fechaNacimiento: Date;
+    equipo: number;
+    no_h: number;
+    no_m: number;
+    salud: number;
+    etapa: number;
+    especie: number;
+}
